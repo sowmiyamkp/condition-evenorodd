5a94eeaa1a7bcb78b236462e27879b10927658e4
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-11 21:10:30 -0800

    add condition-evenorodd

---
+++ main/main.js
@@ -0,0 +1,14 @@
+//program to check if the number is even or odd
+// take input from the user
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number/5) {
+    console.log("The number is visible.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is not.");
+}
+
